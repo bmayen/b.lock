8534907a7fc42beb1ad6fa4f2fe7670e33091941
Mario Gonzalez <mariogonzalez@gmail.com>
2012-05-03 18:19:14 -0400

    Started fleshing out landing screen

---
+++ bLock_iOS/bLock/src/BLKTimeLeftViewController.h
@@ -0,0 +1,14 @@
+//
+//  BLKTimeLeftViewController.h
+//  bLock
+//
+//  Created by Mario Gonzalez on 5/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLKTimeLeftViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *timeDisplay;
+
+@end
---
+++ bLock_iOS/bLock/src/Constants.h
@@ -0,0 +1,18 @@
+//
+//  Constants.h
+//  Freshly
+//
+//  Created by Mario Gonzalez on 12/29/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef __B_LOCK_CONSTANTS_H_
+#define __B_LOCK_CONSTANTS_H_
+
+#define ARC4RANDOM_MAX      0x100000000
+
+// Core data model
+static NSString *const kCoreDataModelNameBikes = @"Bikes";
+
+
+#endif
